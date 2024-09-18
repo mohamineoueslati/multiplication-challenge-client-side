d70f43cbab1f94c92603ee81e8d6dcdb3c5bc4e9
Mohamed Amine Oueslati <amine.oueslati@aderivatives.com>
2024-09-18 13:58:44 +0100

    Update server url

--- src/services/GameService.js
+++ src/services/GameService.js
@@ -1,5 +1,5 @@
 class GameService {
-    static SERVER_URL = 'http://localhost:8081';
+    static SERVER_URL = 'http://localhost:8000';
     static GET_LEADERBOARD = '/leaders';
     static leaderBoard(): Promise<Response> {
         return fetch(`${GameService.SERVER_URL}${GameService.GET_LEADERBOARD}`);
